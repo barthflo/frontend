6b25f64d9415a456021e8bb89eb1d410c813f51a
barthflo <flrnt.barth@gmail.com>
2021-04-18 19:18:42 +0200

    fixed logo jumping

--- src/components/Nav/NavContainer.jsx
+++ src/components/Nav/NavContainer.jsx
@@ -4,16 +4,13 @@ import { useState } from 'react';
 
 const NavContainer = () => {
 	const [showMenu, setShowMenu] = useState(false);
-	const [positionFixed, setPositionFixed] = useState(false);
 	const [background, setBackground] = useState(false);
 	const handleShow = () => setShowMenu(true);
 	const closeMenu = () => setShowMenu(false);
 	const position = () => {
 		if (window.scrollY > 24) {
-			setPositionFixed(true);
 			setBackground(true);
 		} else {
-			setPositionFixed(false);
 			setBackground(false);
 		}
 	};
@@ -22,10 +19,7 @@ const NavContainer = () => {
 	return (
 		<div
 			className={
-				'nav-container d-flex justify-content-center align-items-center flex-wrap justify-content-sm-around w-100 pb-4 ' +
-				(positionFixed
-					? `position-fixed mt-0 pt-0 pt-sm-3`
-					: `position-relative pt-3`) +
+				'nav-container d-flex justify-content-center align-items-center flex-wrap justify-content-sm-around w-100 pb-4 pt-3 position-fixed mt-0' +
 				(background && showMenu ? ' nav-background' : ' ')
 			}
 			onMouseLeave={() => setShowMenu(false)}

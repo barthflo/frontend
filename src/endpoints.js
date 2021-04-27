c024cc744af59c375fa3af41c30ffbbd6c4fa15f
barthflo <flrnt.barth@gmail.com>
2021-04-27 21:53:51 +0200

    add front endpoint for prod

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,4 +1,7 @@
-const FRONTEND = 'http://localhost:3000';
+const FRONTEND =
+	process.env.NODE_ENV === 'production'
+		? 'https://flobarth-webdev.netlify.app'
+		: 'http://localhost:3000';
 const BACKEND =
 	process.env.NODE_ENV === 'production'
 		? 'https://portfolioflobarth.herokuapp.com'
